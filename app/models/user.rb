ebc4cf17c073a55d40f5311dd31a109bde30bf5e
gmgn120 <gmgn120@gmail.com>
2021-02-06 05:57:39 +0000

    Add devise

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  # プロフィール画像アップロード
+  mount_uploader :image_id, ImageUploader
+end
