126beadb2cab42c0b007fd8fb6516924cc438843
gmgn120 <gmgn120@gmail.com>
2021-02-06 08:19:17 +0000

    Add users_controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def search
+  end
+  
+  def index
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get '/about' => 'homes#about'
+  resources :users, only: [:show, :edit, :update, :index]
+  get '/users/search' => 'users#search'
 end
