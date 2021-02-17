d2236f88e5afd71ecea4963687862efa42587ba5
gmgn120 <gmgn120@gmail.com>
2021-02-17 03:43:51 +0000

    Change relationship ajax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,21 +3,21 @@ class RelationshipsController < ApplicationController
     current_user.follow(params[:user_id])
     @user = User.find(params[:user_id])
     @user.create_notification_follow(current_user)
-    redirect_to request.referer
+    
   end
 
   def destroy
+    @user = User.find(params[:user_id])
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer
   end
 
   def followings
-     user = User.find(params[:user_id])
-     @users = user.followings
+     @user = User.find(params[:user_id])
+     @users = @user.followings
   end
 
   def followers
-     user = User.find(params[:user_id])
-     @users = user.followers
+     @user = User.find(params[:user_id])
+     @users = @user.followers
   end
 end
