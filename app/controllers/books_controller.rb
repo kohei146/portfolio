a629f21534a00bf74defaf87e88e3496d2ad0bc1
gmgn120 <gmgn120@gmail.com>
2021-02-17 05:56:59 +0000

    Change books/destroy ajax

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,13 +27,13 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
-    redirect_to book_path(@book.id)
+    
   end
 
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to user_path(current_user.id)
+    @book = Book.find(params[:id])
+    @book.destroy
+    
   end
 
   def search
