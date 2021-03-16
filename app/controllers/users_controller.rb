49307c254fccdaf7c021807df6c264cec26a696f
gmgn120 <gmgn120@gmail.com>
2021-03-16 07:13:11 +0000

    Fix books/show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
   end
   # タイムライン
   def index
-    @books = Book.where(user_id: [current_user.id, * current_user.following_ids]).order(created_at: :desc).page(params[:page]).without_count.per(1)
+    @books = Book.where(user_id: [current_user.id, * current_user.following_ids]).order(created_at: :desc)
   end
 
   private
